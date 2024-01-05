624a2fca613b295418c96ec9f35de7506c0d0fa3
talha <talhaaly103@gmail.com>
2024-01-05 11:40:02 +0500

    working

--- login/app.js
+++ login/app.js
@@ -1,15 +1,23 @@
 const email = document.getElementById('email');
 const Password = document.getElementById('Password');
 const loginBtn = document.getElementById('loginBtn');
+const clickHere = document.getElementById('clickHere')
 
-const 
-loginHandler = () => {
-    if( !email.value && !Password.value){
-    alert('Fill Out Remaining Fields')
-    }else {
-        alert('Login successfully, Now you are diverting to Home page.')
-        window.location.href = '../Home/home.html'
+const
+    loginHandler = () => {
+        if (!email.value && !Password.value) {
+            alert('Fill Out Remaining Fields')
+        } else {
+            alert('Login successfully, Now you are diverting to Home page.')
+            window.location.href = '../Home/home.html'
+        }
     }
-    }
-    
-    loginBtn.addEventListener('click', loginHandler );
+
+loginBtn.addEventListener('click', loginHandler);
+
+
+function clickeHereHandler() {
+    window.location.href = '../signup/signUp.html'
+}
+
+clickHere.addEventListener('click', clickeHereHandler)
